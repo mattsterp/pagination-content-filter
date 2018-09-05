3f5a7fa5cb68edb82ef0111b85650b8aadf3ff38
Matthew Peters <mattsterp@gmail.com>
2018-09-05 09:52:03 -0400

    created hideItems function and showpage function

--- js/script.js
+++ js/script.js
@@ -5,17 +5,21 @@ FSJS project 2 - List Filter and Pagination
 
 // Add variables that store DOM elements you will need to reference and/or manipulate
 const items = document.querySelectorAll('.student-item');
+const itemsPePage = 10;
 
 // Create a function to hide all of the items in the list except for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
 const hideItems = () => {
-	for (let i = 10; i < items.length; i++) {
+	for (let i = itemsPePage; i < items.length; i++) {
 		items[i].style.display = 'none';
 	}
 };
 
-hideItems();
+const showPage = () => {
+	hideItems();
+};
 
+showPage();
 // Create and append the pagination links - Creating a function that can do this is a good approach
 
 // Add functionality to the pagination buttons so that they show and hide the correct items
